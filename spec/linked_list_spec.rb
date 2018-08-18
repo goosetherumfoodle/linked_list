c40e381227e097e0e6c3d3a847713a7422005516
jesse <goosetherumfoodle@users.noreply.github.com>
2018-08-18 19:29:49 -0400

    initial commit

---
+++ spec/linked_list_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe LinkedList do
+  it "has a version number" do
+    expect(LinkedList::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
